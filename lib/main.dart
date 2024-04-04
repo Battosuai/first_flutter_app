4be0fe29b8c781ee21790c1986e466953670134a
Battousai <imdjiban@gmail.com>
2024-04-05 01:24:09 +0600

    Dice roll done

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_container.dart';
+
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 196, 82, 48),
-              Color.fromARGB(255, 187, 11, 26)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: const Center(
-            child: Text('Hello World!'),
-          ),
-        ),
-      ),
+          body: GradientContainer(Color.fromARGB(255, 196, 82, 48),
+              Color.fromARGB(255, 187, 11, 26))),
     ),
   );
 }
